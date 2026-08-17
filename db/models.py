a782969081abd8a706fe97842d67a501984fe9a1
Dr-Kate <katia.01.01.2007d@gmail.com>
2026-08-17 20:50:42 +0300

    fixing-mistakes-in-task

--- db/models.py
+++ db/models.py
@@ -20,8 +20,8 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = ManyToManyField(Actor, related_name='movies')
-    genres = ManyToManyField(Genre, related_name='movies')
+    actors = ManyToManyField(Actor, related_name="movies")
+    genres = ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
         return self.title
@@ -36,7 +36,7 @@ class CinemaHall(models.Model):
         return self.name
 
     @property
-    def capacity(self):
+    def capacity(self) -> int | None:
         return self.rows * self.seats_in_row
 
 
@@ -45,5 +45,5 @@ class MovieSession(models.Model):
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.movie} {self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
