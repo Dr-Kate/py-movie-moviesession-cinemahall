f7a4c94c47661a2cf753c41489f8cf5eec69e819
Dr-Kate <katia.01.01.2007d@gmail.com>
2026-08-18 15:27:38 +0300

    fixing-styles-errors

--- db/models.py
+++ db/models.py
@@ -20,8 +20,8 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = ManyToManyField(Actor, related_name="movies")
-    genres = ManyToManyField(Genre, related_name="movies")
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
         return self.title
@@ -36,7 +36,7 @@ class CinemaHall(models.Model):
         return self.name
 
     @property
-    def capacity(self) -> int | None:
+    def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
 
